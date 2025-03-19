b82c8857c28f487c3f2af2720e955c58ae1e5a76
Bryan Vargas Pacheco <bryan@pop-os.localdomain>
2025-03-18 18:43:47 -0600

    feat:actualizacion de metodos en comentarios

--- src/main/java/org/generation/proyecto/integrador/controller/CommentController.java
+++ src/main/java/org/generation/proyecto/integrador/controller/CommentController.java
@@ -9,27 +9,46 @@ import org.generation.proyecto.integrador.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+
+
+
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/v1/comments")
+@RequestMapping("/api/comments")
 public class CommentController {
 	
 	@Autowired
 	CommentService commentService;
 	
-	@GetMapping("/comenta")
-    public ResponseEntity<Iterator<Comment>> comentar(@RequestParam (name = "curso_id") Long idCurso){
-		return  ResponseEntity.ok(commentService.createComment());
+	@GetMapping("/doComment")
+    public ResponseEntity<?> doComment(@RequestParam (name = "curso_id") Long idCurso){
+		commentService.createComment(null);
+		return  ResponseEntity.ok("");
 	} 
 	
-	@GetMapping("/obtener-comentarios")
-    public ResponseEntity<Iterator<Comment>> obtenerComentarios(@RequestParam (name = "curso_id") Long idCurso){
-		return  ResponseEntity.ok(commentService.getAllComments());
+	@GetMapping("/obtainComment/{curso_id}")
+    public Page<Comment> obtainComment(@PathVariable("curso_id") Long idCurso){
+		return commentService.getAllComments();
+	}
+
+	@PutMapping("deleteComment/{comentario_id}")
+	public ResponseEntity<?> deleteComment(@PathVariable String comentario_id) {
+		//TODO: process PUT request
+		
+		return ResponseEntity.ok("");
 	}
 	
 	
